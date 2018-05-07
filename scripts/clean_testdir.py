77bf3bc9f878465154d0272b502181b8cdca7eb9
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-07 11:47:07 -0700

    adding e3sm_diags_env_cleanup

--- scripts/clean_testdir.py
+++ scripts/clean_testdir.py
@@ -36,8 +36,7 @@ for a_file in os.listdir(testdir):
         print("Removing {f}".format(f=the_file))
         #shutil.rmtree(the_file)
         # os.unlink(the_file)
-        if os.path.isdir(the_file):
-            cmd = "/bin/rm -rf {to_be_removed}".format(to_be_removed=the_file)
-            #cmd = "ls -l {to_be_removed}".format(to_be_removed=the_file)
-            ret_code = run_cmd(cmd, True, False, True)
+        #if os.path.isdir(the_file):
+            #cmd = "/bin/rm -rf {to_be_removed}".format(to_be_removed=the_file)
+            #ret_code = run_cmd(cmd, True, False, True)
     
