934af59584f3a45aae6c5ac7d1ff21af5f7a1e21
Muryanto <muryanto1@ML-9516678.llnl.gov>
2018-05-21 09:27:53 -0700

    make sure __check_test_results() returns failure if no html is generated

--- scripts/e3sm_diags/run_e3sm_diags_tests.py
+++ scripts/e3sm_diags/run_e3sm_diags_tests.py
@@ -45,7 +45,7 @@ sys.stdout.flush()
 cmd = "tail -10 /var/www/acme/acme-diags/e3sm_diags_jenkins/acme_diags_conda_create_system_2018.05.21-09:06:20.out | grep \"Viewer HTML generated at:\""
 ret_code = run_cmd(cmd, True, True, True)
 print("xxx xxx xxx xxx xxx CMD: {c}, ret_code: {r}".format(c=cmd, 
-                                                           r=ret_code)
+                                                           r=ret_code))
 sys.exit(0)
 
 
